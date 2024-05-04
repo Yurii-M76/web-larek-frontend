4dc2760c167a2629e0906ed04b10228cd4b19779
Yurii M <ispoyar@yandex.ru>
2024-05-04 19:06:41 +0300

    src/types/index и readme.md - добавлены интерфейсы

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,31 @@
 import './scss/styles.scss';
+import { Api, ApiListResponse } from './components/base/api';
+import { API_URL } from './utils/constants';
+import { ensureElement } from './utils/utils';
+import { EventEmitter, IEvents } from "./components/base/events";
+import { IProduct } from './types';
 
+// Все шаблоны
+const cardTemplate = ensureElement<HTMLTemplateElement>('#card-catalog');
+cardTemplate.content.cloneNode(true);
+
+const cardPreviewTemplate = ensureElement<HTMLTemplateElement>('#card-preview');
+const cardBasketTemplate = ensureElement<HTMLTemplateElement>('#card-basket');
+const basketTemplate = ensureElement<HTMLTemplateElement>('#basket');
+const orderTemplate = ensureElement<HTMLTemplateElement>('#order');
+const contactsTemplate = ensureElement<HTMLTemplateElement>('#contacts');
+const successTemplate = ensureElement<HTMLTemplateElement>('#success');
+const gallery = ensureElement<HTMLElement>('.gallery');
+
+
+const api = new Api(API_URL);
+const events = new EventEmitter();
+
+// Получаем каталог продуктов с сервера
+api.get('/product')
+	.then((result: ApiListResponse<IProduct>) => {
+		console.log(result.items);
+	})
+	.catch(err => {
+			console.error(err);
+	});
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,48 +1,108 @@
 import { IEvents } from "../components/base/events";
 
-/** Интерфейс данных товара */
-export interface IProductData {
+/** Интерфейс данных товаров в Api */
+export interface IProduct {
 	id: string;
 	title: string;
 	description: string;
 	image: string;
-	category: 'софт-скил' | 'хард-скил' | 'другое' | 'дополнительное' | 'кнопка';
+	category: string;
 	price: number | null;
 }
 
 /** Интерфейс товаров в каталоге*/
-export interface IProductsList {
-	products: IProductData[];
-	preview: string | null;
+export interface IProductList {
+	items: IProduct[];
 	events: IEvents;
-	get(data: string): IProductData;
+	getList(data: IProduct[]): IProduct[];
 }
+
+/** Тип id товара */
+export type TProductId = Pick<IProduct, 'id'>;
+
+/** Тип списка товаров */
+export type TProductItems = Pick<IProductList, 'items'>
+
+/** Тип данных товара на странице */
+export type TProductItem = Pick<IProduct, 'category' | 'title' | 'image' | 'price'>;
 
 /** Интерфейс корзины */
 export interface IBasket {
-	items: Map<string, number>; 
-    total: number;
+	items: IProduct[]; 
+  total: number;
 	events: IEvents;
-	add(id: string): void;
-	remove(id: string): void;
-	disableButton(total: number): boolean;
+	add(item: TProductId): void;
+	remove(item: TProductId): void;
+	getTotal(...items: IProduct[]): number;
+	check(item: TProductId): boolean;
+	disableButton(items: IProduct[]): boolean;
 	clear(): void;
 }
 
+/** Тип данных для модального окна успешного оформления заказа */
+export type TSuccess = Pick<IBasket, 'total'>;
+
 /** Интерфейс формы заказа */
 export interface IOrder {
-    payment: 'card' | 'cash';
+	items: IProduct[];
+  payment: TPaymentMethod;
 	address: string;
-    email: string;
-    phone: string;
+  email: string;
+  phone: string;
 	checkValidation(data: Record<keyof TOrderContacts, string>): boolean;
 }
-
-/** Тип данных товара на странице */
-export type TProductItem = Pick<IProductData, 'category' | 'title' | 'image' | 'price'>;
 
 /** Тип данных для валидации формы оформления заказа */
 export type TOrderContacts = Pick<IOrder, 'address' | 'email' | 'phone'>;
 
-/** Тип данных для модального окна успешного оформления заказа */
-export type TSuccessOrder = Pick<IBasket, 'total'>;
+/** Тип способа оплаты при оформления заказа */
+export type TPaymentMethod = 'card' | 'cash';
+
+/** Интерфейся класса создания карточки товара */
+export interface IProductCard {
+	category: HTMLElement;
+	title: HTMLElement;
+	image: HTMLElement;
+	price: HTMLElement;
+	set(data: IProduct): void;
+	render(): HTMLElement;
+}
+
+/** Интерфейс абстрактного класса Modal */
+export interface IModal {
+	modal: HTMLElement;
+	events: IEvents;
+	open(): void;
+	close(): void;
+}
+
+/** Интерфейс модального окна с полной информацией о товаре */
+export interface IModalPreviewProduct {
+	category: HTMLElement;
+	title: HTMLElement;
+	image: HTMLElement;
+	price: HTMLElement;
+	description: HTMLElement;
+	events: IEvents;
+}
+
+/** Интерфейс модального окна формы оформления заказа */
+export interface IModalPreviewProduct {
+	paymentMethod: HTMLElement;
+	form: HTMLFormElement;
+	address: HTMLElement;
+	email: HTMLElement;
+	phone: HTMLElement;
+	button: HTMLButtonElement;
+	errors: HTMLElement;
+	showError(element: HTMLElement): void;
+	hideError(field: string): void;
+	close(): void;
+}
+
+/** Интерфейс модального окна успешного оформления заказа */
+export interface IModalPreviewProduct {
+	total: HTMLElement;
+	button: HTMLButtonElement;
+	getTotal(items: IProduct[]): number;
+}
